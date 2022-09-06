5df47dd83a2c0539295a0dd0f61689e25648b195
Tanish1107 <72192376+Tanish1107@users.noreply.github.com>
2022-09-07 00:01:00 +0530

    Update action.js

--- action.js
+++ action.js
@@ -1,10 +1,10 @@
-var j=1;
+var j=1;var k=1;
 function prp(){
     var boxd=document.getElementById("td1");
     var box=document.createElement("div");
     box.className="box";
-     box.id=k;
-     k++;
+    box.id=k;
+    k++;
    
     var TITLE= String(prompt("ENTER THE TITLE"));
     var items=Number(prompt("ENTER NUMBER OF ITEMS"))
@@ -14,8 +14,8 @@ function prp(){
         arr[i]=prompt("ENTER "+(i)+" ELEMENT");
     }
     
-    box.innerHTML+=`<h2>${TITLE}</h2>`;
-     box.innerHTML += `<button style="margin-left:40px ;
+    box.innerHTML+=`<h2 >${TITLE}</h2>`;
+    box.innerHTML += `<button style="margin-left:40px ;
                          background-color:#EC7272 ; 
                          font-size:12px ; 
                          font-weight:bold ; 
@@ -23,7 +23,7 @@ function prp(){
                          border-radius:14px " 
                          onclick=removeBox(${box.id}) >Delete</button> <hr>`;
     for(var i=1;i<items+1;i++){
-            box.innerHTML+=`<h4>
+            box.innerHTML+=`<h4 >
                 <input type="checkbox" onclick="stride()" id="id${j}">
                 <label for="id${j}" id="label${j}">${arr[i]}</label>    
                 </h4>`;
